a253caaf7133e66517465cff25bd38061b4c4a56
rgdevelopings <63463686+rahulg009@users.noreply.github.com>
2021-06-28 14:25:05 +0530

    hash

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
-const User = mongoose.model('User', {
+const userSchmea = new mongoose.Schema( {
     name: {
         type: String,
         required: true,
@@ -38,6 +39,17 @@ const User = mongoose.model('User', {
             }
         }
     }
-})
+});
+
+userSchmea.pre('save',async function(next){
+    const user = this
+
+    if(user.isModified('password')){
+        user.password= await bcrypt.hash(user.password,8)
+    }
+    next()
+});
+
+const User = mongoose.model('User',userSchmea);
 
 module.exports = User
