7fa4da582b6e4255e90122546d9269f23f7bcd67
rgdevelopings <63463686+rahulg009@users.noreply.github.com>
2021-07-02 11:03:40 +0530

    Update user.js

--- src/routers/user.js
+++ src/routers/user.js
@@ -54,21 +54,6 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
 
 router.patch('/users/:id', async (req, res) => {
     const updates = Object.keys(req.body)
@@ -95,15 +80,10 @@ router.patch('/users/:id', async (req, res) => {
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me',auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     } catch (e) {
         res.status(500).send()
     }
