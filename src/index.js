01a4afbd40d7c72eb07b0e600639e5edda071c95
rgdevelopings <63463686+rahulg009@users.noreply.github.com>
2021-07-04 00:16:39 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
